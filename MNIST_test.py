0e710609057e3332723530a990c081ca1a006b30
benb1028 <59970631+benb1028@users.noreply.github.com>
2020-04-20 10:51:40 -0400

    Fully working algorithm
    Added a function to plot results, streamlined code.

--- MNIST_test.py
+++ MNIST_test.py
@@ -2,121 +2,113 @@
 # Created for CS449 Assignment 3 at Clarkson University 
 # Adaboost test
 
+
+# max_depth for decision tree estimator needs to be at least 4. After testing,
+# max_depth = 10 provided near maximum testing accuracy without overfitting.
+
+# learn_rate can be from 0-1, but high values are not effective. After testing,
+# learn_rate = 0.05 was chosen, but rates from 0.1 to 0.0001 provided similar test accuracy.
+
 import numpy as np
-from adaboost import Adaboost
+
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
-def accuracy(y, y_pred):
-    acc = np.sum(y == y_pred) / len(y)
-    return acc
+from adaboost import make_result_figure
 
-def error(y, y_pred):
-    err = np.sum(y != y_pred) / len(y)
-    return err
-
-def make_comparison_plot(max_classifiers, X, y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
-    tree_model = DecisionTreeClassifier(max_depth = 10)
-    svc_model = SVC(C = 1.)
-    
-    max_classifiers = 25
-    n_classifiers = list(range(15, max_classifiers))
-    
-    tree_acc_list = []
-    tree_error_rate_list = []
-    
-    
-    svc_acc_list = []
-    svc_error_rate_list = []
-    
-    skl_default_acc_list = []
-    skl_default_error_rate_list = []
-    
-    for n in n_classifiers:
-        
-        tree_boost = Adaboost(learner = tree_model, n_classifiers = n)
-        tree_boost.fit(X_train, y_train)
-        tree_boost.predict(X_test, y_test)
-        tree_acc, tree_error_rate = tree_boost.results()
-        tree_acc_list.append(tree_acc)
-        tree_error_rate_list.append(tree_error_rate)
-    
-        
-        svc_boost = Adaboost(learner = svc_model, n_classifiers = n)
-        svc_boost.fit(X_train, y_train)
-        svc_boost.predict(X_test, y_test)
-        svc_acc, svc_error_rate = svc_boost.results()
-        svc_acc_list.append(svc_acc)
-        svc_error_rate_list.append(svc_error_rate)
-    
-    
-        skl_default_boost = AdaBoostClassifier(tree_model, n_estimators = n)
-        skl_default_boost.fit(X_train, y_train)
-        y_pred = skl_default_boost.predict(X_test)    
-        skl_default_acc = accuracy(y_test, y_pred)
-        skl_default_error_rate = error(y_test, y_pred)
-        skl_default_acc_list.append(skl_default_acc)
-        skl_default_error_rate_list.append(skl_default_error_rate)
-    
-        
-        print(f'Iteration {n} / {max_classifiers}')
-        
-    tree_max_acc = max(tree_acc_list)
-    tree_min_error = min(tree_error_rate_list)
-    svc_max_acc = max(svc_acc_list)
-    svc_min_error = min(svc_error_rate_list)
-    skl_default_max_acc = max(skl_default_acc_list)
-    skl_default_min_error = min(skl_default_error_rate_list)
-    print("Best Accuracies by Model:")
-    print(f'Tree Model: {tree_max_acc:.4%} with {tree_acc_list.index(max(tree_acc_list))} weak classifiers')
-    print(f'SVC Model: {svc_max_acc:.4%} with {svc_acc_list.index(max(svc_acc_list))} weak classifiers')
-    print(f'Default Model: {skl_default_max_acc:.4%} with {skl_default_acc_list.index(max(skl_default_acc_list))} weak classifiers')
-    fig1 = plt.figure()
-    ax = fig1.add_subplot(1,1,1)
-    ax.spines['top'].set_color('none')
-    ax.spines['bottom'].set_color('none')
-    ax.spines['left'].set_color('none')
-    ax.spines['right'].set_color('none')
-    ax.tick_params(labelcolor='w', top=False, bottom=False, left=False, right=False)
-    ax.set_xlabel('Number of Classifiers')
-    
-    ax1 = fig1.add_subplot(2,1,1)
-    ax2 = fig1.add_subplot(2,1,2)
-    
-    ax1.plot(n_classifiers, tree_acc_list, 'g-', n_classifiers, svc_acc_list, 'b-', n_classifiers, skl_default_acc_list, 'r-',)
-    ax2.plot(n_classifiers, tree_error_rate_list, 'g-', n_classifiers, svc_error_rate_list, 'b-', n_classifiers, skl_default_error_rate_list, 'r-',)
-    
-    
-    ax1.set_ylabel('Test Accuracy')
-    ax1.set_title('Testing Accuracy of Compared Models')
-    ax2.set_ylabel('Test Error Rate')
-    ax2.set_title('Test Error Rate of Compared Models')
-    #fig1.legend((tree_acc_list, svc_acc_list, skl_default_acc_list),
-    #           ('Our Algorithm - Binary Tree Classifier', 'Our Algorithm - SVC',
-    #            'Built-in AdaBoost with Binary Tree Classifier'))
-    fig1.show()
-
-
-classes = list(range(0,10))
 digits = datasets.load_digits()  
-X = digits['data']
-y = digits['target']
-max_depth = 15
-depths = list(range(max_depth))[1:]
-print(depths)
+MNIST_X = digits['data']
+MNIST_y = digits['target']
+
+MNIST_fig = make_result_figure('MNIST Handwritten Digits', MNIST_X, MNIST_y)
+MNIST_fig.show()
+
+'''
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
-for depth in depths:
-    boost = Adaboost(learner = DecisionTreeClassifier(max_depth = depth), n_classifiers = 100, learn_rate = .01)
-    boost.fit(X_train, y_train)
-    boost.predict(X_test, y_test)
-    acc, classifier_errors = boost.results()
-    print(f'Tree depth: {depth} : Accuracy: {acc:.4%}')
-    print('Classifier Errors:')
-    print(classifier_errors)
+max_depth = 10
+learn_rate = 0.05
+n_classifiers = 80
+max_itr = 1800
+
+#accs = compare_learn_rates(X = X, y = y, learn_rates = learn_rates, num_cycles = num_cycles, max_depth = max_depth)
+tree_base = DecisionTreeClassifier(max_depth = max_depth)
+logreg_base = LogisticRegression(max_iter = max_itr)
+
+custom_tree_model = Adaboost(learner = tree_base, n_classifiers = n_classifiers, learn_rate = learn_rate).fit(X_train, y_train)
+custom_tree_model.predict(X_test, y_test)
+ctm_acc, ctm_train_error = custom_tree_model.results()
+ctm_train_error = fix_train_error(ctm_train_error)
+
+custom_logreg_model = Adaboost(learner = logreg_base, n_classifiers = n_classifiers, learn_rate = learn_rate).fit(X_train, y_train)
+custom_logreg_model.predict(X_test, y_test)
+clrm_acc, clrm_train_error = custom_logreg_model.results()
+clrm_train_error = fix_train_error(clrm_train_error)
+
+skl_tree_model = AdaBoostClassifier(tree_base, n_estimators = n_classifiers, learning_rate = learn_rate).fit(X_train, y_train)
+skltm_acc = skl_tree_model.score(X_test, y_test)
+skltm_train_error = fix_train_error(skl_tree_model.estimator_errors_)
+
+skl_logreg_model = AdaBoostClassifier(logreg_base, n_estimators = n_classifiers, learning_rate = learn_rate, algorithm = 'SAMME').fit(X_train, y_train)
+skllrm_acc = skl_logreg_model.score(X_test, y_test)
+skllrm_train_error = fix_train_error(skl_logreg_model.estimator_errors_)
+
+models = {'0':"Custom Implementation with binary tree classifier",
+          '1':"Custom Implementation with logistic regression classifier",
+          '2':"SKLearn Implementation with binary tree classifier",
+          '3':"SKLearn Implementation with logistic regression classifier"}
+labels = [f'Model {i}: {label}' for (i, label) in models.items()]
+legend_str = '\n'.join([label for label in labels])
+accuracies = [ctm_acc, clrm_acc, skltm_acc, skllrm_acc]
+avg_acc = np.mean(accuracies)
+classifiers = np.arange(1, n_classifiers + 1)
+param_string = f"* All models trained with learning rate of {learn_rate}. Tree classifiers have max depth of {max_depth}. Regression classifiers have {max_itr} maximum iterations."
+
+grid_size = (4, 6)
+fig = plt.figure(figsize=(18, 12))
+ax1 = plt.subplot2grid(grid_size, (0,0), colspan = 3, rowspan = 3)
+ax2 = plt.subplot2grid(grid_size, (0,3), colspan = 3, rowspan = 3)
+ax3 = plt.subplot2grid(grid_size, (3,0), colspan = 6, rowspan = 1)
+
+ax1.plot(classifiers, ctm_train_error, marker = 'o', markersize = 16, color = 'r', linewidth = 0.75, label = 'Model 0', alpha = 0.75)
+ax1.plot(classifiers, clrm_train_error, marker = '+', markersize = 16, color = 'r', linewidth = 0.75, label = 'Model 1')
+ax1.plot(classifiers, skltm_train_error, marker = 'o', markersize = 16, color = 'b', linewidth = 0.75, label = 'Model 2', alpha = 0.75)
+ax1.plot(classifiers, skllrm_train_error, marker = 'x', markersize = 16, color = 'b', linewidth = 0.75, label = 'Model 3')
+
+ax1.set_title('Training Error vs. # of Classifiers', fontsize = 18)
+ax1.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+ax1.set_xlabel('Classifiers', fontsize = 16)
+ax1.set_ylabel('Error', fontsize = 16)
+ax1.legend(loc = 'upper right', fontsize = 16)
+
+ax2.bar(list(models.keys()), accuracies, color = ['r', 'r', 'b', 'b'])
+ax2.plot([-0.5, 3.5], [avg_acc]*2, marker = '', color = 'k', linestyle = 'dashed', linewidth = 4.0)
+ax2.text(-0.62, avg_acc + 0.02, f'Average = {avg_acc:.2%}', fontsize = 16)
+
+ax2.set_title('Testing Accuracy of Models', fontsize = 18)
+ax2.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+ax2.set_xlabel('Model', fontsize = 16)
+ax2.set_ylabel('Accuracy', fontsize = 16)
+
+ax3.set_title('Models:', fontsize = 18)
+ax3.axis([0, 1, 0, 1])
+ax3.get_xaxis().set_visible(False)
+ax3.get_yaxis().set_visible(False)
+
+ax3.text(0.05, 0.80, labels[0], transform=ax3.transAxes, fontsize = 14)
+ax3.text(0.05, 0.50, labels[1], transform=ax3.transAxes, fontsize = 14)
+ax3.text(0.55, 0.80, labels[2], transform=ax3.transAxes, fontsize = 14)
+ax3.text(0.55, 0.50, labels[3], transform=ax3.transAxes, fontsize = 14)
+ax3.text(0.50, 0.10, param_string, transform=ax3.transAxes, fontsize = 14, horizontalalignment = 'center')
+
+fig.suptitle('MNIST Handwritten Digit Test: Results', x = 0.5, y = 1.03, fontsize = 24)
+fig.tight_layout(pad = 0.5, w_pad = 0.5, h_pad = 0.5)
+fig.show()
+'''
+
--- adaboost.py
+++ adaboost.py
@@ -3,10 +3,13 @@
 # Adaboost Implementation
 
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import AdaBoostClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
-
-stump = DecisionTreeClassifier(max_depth = 1)
 
 class CLF:
     '''Generic classifier to copy'''
@@ -22,7 +25,7 @@ class Adaboost:
     relevent data for analysis and prediction.
     '''
     
-    def __init__(self, learner, n_classifiers = 20, learn_rate = 1):
+    def __init__(self, learner, n_classifiers = 20, learn_rate = 0.1):
         '''Initialize the class with optional parameters.'''
         self.n_classifiers = n_classifiers
         self.classifiers = []
@@ -36,8 +39,8 @@ class Adaboost:
         if learn_rate > 1 or learn_rate <=0:
             print("Warning: invalid learning rate. Learn rate has been set to 1.")
             self.learn_rate = 1
-        self.learn_rate = learn_rate
-
+        else:
+            self.learn_rate = learn_rate
         
     def fit(self, X, y):
         '''Fit the model to a training sample.
@@ -69,7 +72,8 @@ class Adaboost:
             
             if idx < self.n_classifiers - 1:
                 w /= wsum
-
+        return self
+    
     def makeClassifier(self):
         '''Initialize a copy of the weak classifier'''
         clf = CLF(self.learner)
@@ -168,3 +172,110 @@ class Adaboost:
     def check(self, y):
         self.accuracy = np.sum(np.where(self.predictions == y, 1, 0)) / len(self.predictions)
         self.error_rate = np.sum(np.where(self.predictions != y, 1, 0)) / len(self.predictions)
+        
+        
+        
+def compare_learn_rates(X, y, learn_rates, num_cycles, max_depth):
+    acc_mat = np.zeros((num_cycles, len(learn_rates)))
+    for cycle in range(num_cycles):
+        print(f'Starting cycle {cycle}...')
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2) # resplits the data every cycle to randomize results
+        for idx, rate in enumerate(learn_rates):
+            
+            boost = Adaboost(learner = DecisionTreeClassifier(max_depth = max_depth), n_classifiers = 100, learn_rate = rate)
+            boost.fit(X_train, y_train)
+            boost.predict(X_test, y_test)
+            acc, _ = boost.results()
+            acc_mat[cycle, idx] = acc
+            #print(f'Learn Rate: {rate} : Accuracy: {acc:.4%}')
+    avg_acc = np.mean(acc_mat, axis = 0)
+    wins = np.argmax(acc_mat, axis = 1)
+    lr_idx, win_cnt = np.unique(wins, return_counts = True)
+    win_cnt_dict = dict(zip(lr_idx, win_cnt))
+    for idx, rate in enumerate(learn_rates):
+        print(f'Learn Rate = {rate}:')
+        print(f'Average Accuracy: {avg_acc[idx]:.4%}')
+        print(f'Had the best accuracy {win_cnt_dict.get(idx, 0)} times.\n')
+            
+    return avg_acc
+
+def fix_train_error(error_list):
+    fixed_list = [err if err != 1 else 0 for err in error_list]
+    return fixed_list
+
+def make_result_figure(dataset_name, X, y, learn_rate = 0.05, max_depth = 10, max_itr = 1800, n_classifiers = 100):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+    
+    tree_base = DecisionTreeClassifier(max_depth = max_depth)
+    logreg_base = LogisticRegression(max_iter = max_itr)
+    
+    custom_tree_model = Adaboost(learner = tree_base, n_classifiers = n_classifiers, learn_rate = learn_rate).fit(X_train, y_train)
+    custom_tree_model.predict(X_test, y_test)
+    ctm_acc, ctm_train_error = custom_tree_model.results()
+    ctm_train_error = fix_train_error(ctm_train_error)
+    
+    custom_logreg_model = Adaboost(learner = logreg_base, n_classifiers = n_classifiers, learn_rate = learn_rate).fit(X_train, y_train)
+    custom_logreg_model.predict(X_test, y_test)
+    clrm_acc, clrm_train_error = custom_logreg_model.results()
+    clrm_train_error = fix_train_error(clrm_train_error)
+    
+    skl_tree_model = AdaBoostClassifier(tree_base, n_estimators = n_classifiers, learning_rate = learn_rate).fit(X_train, y_train)
+    skltm_acc = skl_tree_model.score(X_test, y_test)
+    skltm_train_error = fix_train_error(skl_tree_model.estimator_errors_)
+    
+    skl_logreg_model = AdaBoostClassifier(logreg_base, n_estimators = n_classifiers, learning_rate = learn_rate, algorithm = 'SAMME').fit(X_train, y_train)
+    skllrm_acc = skl_logreg_model.score(X_test, y_test)
+    skllrm_train_error = fix_train_error(skl_logreg_model.estimator_errors_)
+    
+    models = {'0':"Custom Implementation with binary tree classifier",
+              '1':"Custom Implementation with logistic regression classifier",
+              '2':"SKLearn Implementation with binary tree classifier",
+              '3':"SKLearn Implementation with logistic regression classifier"}
+    labels = [f'Model {i}: {label}' for (i, label) in models.items()]
+    legend_str = '\n'.join([label for label in labels])
+    accuracies = [ctm_acc, clrm_acc, skltm_acc, skllrm_acc]
+    avg_acc = np.mean(accuracies)
+    classifiers = np.arange(1, n_classifiers + 1)
+    param_string = f"* All models trained with learning rate of {learn_rate}. Tree classifiers have max depth of {max_depth}. Regression classifiers have {max_itr} maximum iterations."
+    
+    grid_size = (4, 6)
+    fig = plt.figure(figsize=(18, 12))
+    ax1 = plt.subplot2grid(grid_size, (0,0), colspan = 3, rowspan = 3)
+    ax2 = plt.subplot2grid(grid_size, (0,3), colspan = 3, rowspan = 3)
+    ax3 = plt.subplot2grid(grid_size, (3,0), colspan = 6, rowspan = 1)
+    
+    ax1.plot(classifiers, ctm_train_error, marker = 'o', markersize = 16, color = 'r', linewidth = 0.75, label = 'Model 0', alpha = 0.75)
+    ax1.plot(classifiers, clrm_train_error, marker = '+', markersize = 16, color = 'r', linewidth = 0.75, label = 'Model 1')
+    ax1.plot(classifiers, skltm_train_error, marker = 'o', markersize = 16, color = 'b', linewidth = 0.75, label = 'Model 2', alpha = 0.75)
+    ax1.plot(classifiers, skllrm_train_error, marker = 'x', markersize = 16, color = 'b', linewidth = 0.75, label = 'Model 3')
+    
+    ax1.set_title('Training Error vs. # of Classifiers', fontsize = 18)
+    ax1.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    ax1.set_xlabel('Classifiers', fontsize = 16)
+    ax1.set_ylabel('Error', fontsize = 16)
+    ax1.legend(loc = 'upper right', fontsize = 16)
+    
+    ax2.bar(list(models.keys()), accuracies, color = ['r', 'r', 'b', 'b'])
+    ax2.plot([-0.5, 3.5], [avg_acc]*2, marker = '', color = 'k', linestyle = 'dashed', linewidth = 4.0)
+    ax2.text(-0.62, avg_acc + 0.02, f'Average = {avg_acc:.2%}', fontsize = 16)
+    
+    ax2.set_title('Testing Accuracy of Models', fontsize = 18)
+    ax2.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+    ax2.set_xlabel('Model', fontsize = 16)
+    ax2.set_ylabel('Accuracy', fontsize = 16)
+    
+    ax3.set_title('Models:', fontsize = 18)
+    ax3.axis([0, 1, 0, 1])
+    ax3.get_xaxis().set_visible(False)
+    ax3.get_yaxis().set_visible(False)
+    
+    ax3.text(0.05, 0.80, labels[0], transform=ax3.transAxes, fontsize = 14)
+    ax3.text(0.05, 0.50, labels[1], transform=ax3.transAxes, fontsize = 14)
+    ax3.text(0.55, 0.80, labels[2], transform=ax3.transAxes, fontsize = 14)
+    ax3.text(0.55, 0.50, labels[3], transform=ax3.transAxes, fontsize = 14)
+    ax3.text(0.50, 0.10, param_string, transform=ax3.transAxes, fontsize = 14, horizontalalignment = 'center')
+    
+    fig.suptitle(f'{dataset_name} Test: Results', x = 0.5, y = 1.03, fontsize = 24)
+    fig.tight_layout(pad = 0.5, w_pad = 0.5, h_pad = 0.5)
+    
+    return fig
